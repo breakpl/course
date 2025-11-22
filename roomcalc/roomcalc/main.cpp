74766639c76b7f05df3cb88773d685c6a278d63d
Dariusz Kalita <dariusz.kalita@dynatrace.com>
2025-11-22 07:48:19 +0100

    da

--- roomcalc/roomcalc/main.cpp
+++ roomcalc/roomcalc/main.cpp
@@ -18,8 +18,21 @@ int main(int argc, const char * argv[]) {
     std::cin >> roomWidth;
     std::cout << "Enter lenght of the room: ";
     std::cin >> roomLenght;
-    std::cout << roomLenght;
     
     std::cout << "The area of the room is: " << roomWidth * roomLenght << "sq feet." << std::endl;
+        
+    double a = 0.1 + 0.2001;
+//    double a = 0.1 + 0.2000000000001; eq
+    double b = 0.3;
+
+    if (fabs(a - b) < 1e-9) {  // Check if the difference is within a small epsilon
+        std::cout << "a and b are approximately equal" << std::endl;
+    } else {
+        std::cout << "a and b are not equal" << std::endl;
+    }
+
+
+    return 0;
+    
     return EXIT_SUCCESS;
 }
