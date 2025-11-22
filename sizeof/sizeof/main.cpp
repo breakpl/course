5abde0e6715dfcbc3f2d65000a856a3f576e67e9
breakpl <52470902+breakpl@users.noreply.github.com>
2025-11-22 07:46:03 +0100

    blah

---
+++ sizeof/sizeof/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sizeof
+//
+//  Created by Darek Kalita on 22/11/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
