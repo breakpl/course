8167174d0d4fdebe6ae54a722815735396014cda
breakpl <52470902+breakpl@users.noreply.github.com>
2026-01-12 22:39:15 +0100

    blah

--- whichsprint/whichsprint/whichsprintApp.swift
+++ whichsprint/whichsprint/whichsprintApp.swift
@@ -1,16 +1,9 @@
-//
-//  whichsprintApp.swift
-//  whichsprint
-//
-//  Created by Darek Kalita on 11/01/2026.
-//
-
 import SwiftUI
 
 @main
 struct whichsprintApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
+
     var body: some Scene {
         Settings {
             EmptyView()
@@ -23,20 +16,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var popover = NSPopover()
     
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Create the status item in the menu bar
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
+        
         if let button = statusItem?.button {
-            button.title = "311"
+            button.image = createMenuBarImage(text: "311")
             button.action = #selector(togglePopover)
             button.target = self
         }
-
-        // Configure the popover
+        
         popover.contentSize = NSSize(width: 300, height: 200)
         popover.behavior = .transient
         popover.contentViewController = NSHostingController(rootView: ContentView())
     }
+    
     @objc func togglePopover() {
         if let button = statusItem?.button {
             if popover.isShown {
@@ -45,5 +37,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
             }
         }
+    }
+    
+    func createMenuBarImage(text: String) -> NSImage {
+        let image = NSImage(size: NSSize(width: 50, height: 22), flipped: false) { rect in
+            // Draw logo if available
+            if let logo = NSImage(named: "dynatrace_logo") {
+                logo.draw(in: rect)
+            }
+            
+            // Draw text
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: NSFont.systemFont(ofSize: 12, weight: .medium),
+                .foregroundColor: NSColor.white,
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let textRect = NSRect(x: 0, y: -1, width: 50, height: 22)
+            text.draw(in: textRect, withAttributes: attributes)
+            
+            return true
+        }
+        
+        image.isTemplate = false
+        return image
     }
 }
